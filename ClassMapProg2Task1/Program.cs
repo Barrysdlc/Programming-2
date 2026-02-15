41d2eb91c2adc1f95db48f726b012c385f46d049
Barrysdlc <cacarmona01@gmail.com>
2026-02-15 00:33:10 -0400

    Add files via upload
    Adding task 1

---
+++ ClassMapProg2Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using SistemaComunidad;
+
+
+Docente miProfe = new Docente
+{
+    Nombre = "Ing. Starlyn (el jefe)",
+    Especialidad = "Joseador del genero.",
+    Salario = 5000
+};
+
+miProfe.Presentarse();
+Console.WriteLine($"Especialidad: {miProfe.Especialidad}, Salario: ${miProfe.Salario}");
+
+Console.WriteLine("\nPresiona cualquier tecla para salir...");
+Console.ReadKey();
