b938a7f6194602ad3ee96fd8109fddd05d3ab89f
Barrysdlc <cacarmona01@gmail.com>
2026-03-20 21:46:10 -0400

    do task 4

--- UserCRUB API/UserCRUB API/obj/Debug/net8.0/SystemAdminAPI.AssemblyInfo.cs
+++ UserCRUB API/UserCRUB API/obj/Debug/net8.0/SystemAdminAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SystemAdminAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0e26ed78af8c37458ff969d049fe36d00c2c94c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SystemAdminAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SystemAdminAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
